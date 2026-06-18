85d0ed7e6b5ad865a440d6053313313d2640957e
SketchyUrk <andoniouandtread@gmail.com>
2026-06-17 23:14:05 -0700

    defined load_tasks

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ from os import path
 app = FastAPI()
 
 TASKS_FILE = "tasks.json"
+
+class TaskCreate(BaseModel):
+    title: str
+    
+def load_tasks():
+    if not path.exists(TASKS_FILE):
+        return []
+    
+    try:
+        with open(TASKS_FILE, "r") as file:
+            return load(file)
+    except (JSONDecodeError, FileNotFoundError):
+        return []
+    
